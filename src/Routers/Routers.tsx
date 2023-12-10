340bef60c4c79a231842b1f68be61c83ed53762f
Ihor Havryliak <94486546+Ihorhavryliak@users.noreply.github.com>
2023-12-10 12:51:58 +0200

    added page delete aaccount iren

--- src/Routers/Routers.tsx
+++ src/Routers/Routers.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 
 import App from "../App";
 import PrivacyPolicyIren from "../page/PrivacyPolicyIren";
+import DeleteAccountIren from "../page/DeleteAccountIren";
 
 export const Routers = () => {
   return (
@@ -11,6 +12,7 @@ export const Routers = () => {
           <Route index element={<App />} />
         </Route>
         <Route path="/privacy-policy-iren" element={<PrivacyPolicyIren />} />
+        <Route path="/delete-accountIren-iren" element={<DeleteAccountIren />} />
         {/* #order */}
       </Routes>
     </>
---
+++ src/page/DeleteAccountIren.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const DeleteAccountIren = () => {
+  return (
+    <div className="container">
+      <h1>Delete account Iren - Cafee, Restoran App</h1>
+      <p>
+        If you want to delete your account, send us your email address:
+        timebizua@gmail.com
+      </p>
+      <p> Message: Delete account: [Your email] </p>
+    </div>
+  );
+};
+
+export default DeleteAccountIren;
